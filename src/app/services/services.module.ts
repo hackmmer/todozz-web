444f52a0a3d32f3ec6e3e9eec696ee8cd5921e7a
Blizz1800 <blizzsoftword@gmail.com>
2025-03-13 04:47:53 -0400

    using cookies instead of localstorage

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -2,12 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoService } from './todo.service';
 import { UserService } from './user.service';
-import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { StorageService } from './storage.service';
 import { ShareService } from './share.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   imports: [CommonModule],
-  providers: [TodoService, UserService, StorageService, ShareService, provideHttpClient(withInterceptorsFromDi(), withFetch())],
+  providers: [
+    TodoService,
+    UserService,
+    StorageService,
+    ShareService,
+
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+    CookieService,
+  ],
 })
 export class ServicesModule {}
--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,3 +1,4 @@
+import { CookieOptions, CookieService } from 'ngx-cookie-service';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { ThemeEnum } from '../enums/storage';
 import { isPlatformBrowser } from '@angular/common';
@@ -6,46 +7,54 @@ import { isPlatformBrowser } from '@angular/common';
   providedIn: 'root',
 })
 export class StorageService {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  readonly cookiesConfig: CookieOptions = {
+    secure: true,
+    sameSite: 'Strict',
+  };
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private cookies: CookieService
+  ) {}
 
   getTheme() {
     if (!isPlatformBrowser(this.platformId)) return null;
-    return sessionStorage.getItem('theme');
+    return this.getItem('theme');
   }
 
   setTheme(theme: ThemeEnum) {
     if (!isPlatformBrowser(this.platformId)) return null;
-    return sessionStorage.setItem('theme', theme.toString());
+    return this.setItem('theme', theme.toString());
   }
 
   getSession() {
     if (!isPlatformBrowser(this.platformId)) return null;
-    return sessionStorage.getItem('Session');
+    return this.getItem('Session');
   }
 
   setSession(session: string) {
     if (!isPlatformBrowser(this.platformId)) return;
-    sessionStorage.setItem('Session', session);
+    this.setItem('Session', session);
   }
 
   setItem(key: string, value: string) {
-    sessionStorage.setItem(key, value);
+    this.cookies.set(key, value, this.cookiesConfig);
   }
 
   setItems(obj: any) {
     if (!isPlatformBrowser(this.platformId)) return;
     Object.keys(obj).forEach((e) => {
-      sessionStorage.setItem(e, obj[e]);
+      this.setItem(e, obj[e]);
     });
   }
 
   getItem(key: string) {
     if (!isPlatformBrowser(this.platformId)) return;
-    sessionStorage.getItem(key);
+    return this.cookies.get(key);
   }
 
   clear() {
     if (!isPlatformBrowser(this.platformId)) return;
-    sessionStorage.clear();
+    this.cookies.deleteAll();
   }
 }
